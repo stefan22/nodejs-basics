726e0fb91a8bc451cc8b067f07ad3e7eac3957d2
Stefan Lissia <stefan.emailacct@gmail.com>
2015-09-17 21:28:16 +0100

    implementing js

--- avion/index.js
+++ avion/index.js
@@ -1,6 +1,5 @@
-module.exports = function(info) {
-
-	var values = {
+var Flight = function() {
+	this.data = {
 		number: null,
 		origin: null,
 		destination: null,
@@ -10,31 +9,37 @@ module.exports = function(info) {
 		actualArrive: null
 	};
 
-
-	for ( var prop in values) {
-		if ( values[prop] != 'undefined') {
-			values[prop] = info[prop];
+	this.fill = function(info) {
+		for ( var prop in this.data) {
+			if ( this.data[prop] != 'undefined') {
+				 this.data[prop] = info[prop];
+			}
 		}
-	}
-
-
-	var functions = {
-		triggerDepart: function() {
-			values.actualDepart = Date.now();
-		},
-	
-		triggerArrive: function() {
-			values.actualArrive = Date.now();
-		},
-
-		getInformation: function() {
-			return values;
-		}
-
 	};
 
+	this.triggerDepart = function() {
+		this.data.actualDepart = Date.now();
+	},
+	
+	this.triggerArrive = function() {
+		this.data.actualArrive = Date.now();
+	},
 
-	return functions;
+	this.getInformation = function() {
+		return this.data;
+	}
 
+};
+
+
+
+
+
+module.exports = function(info) {
+	var instance = new Flight();
+
+	instance.fill(info);
+
+	return instance;	
 
 };
