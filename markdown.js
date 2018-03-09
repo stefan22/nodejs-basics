9bc6d2d609e7d93a0c7b3bbf89cd3b3f30a8e540
stefan22 <stefan.emailacct@gmail.com>
2018-03-09 00:14:38 +0000

    removed-old-files

--- markdown.js
+++
@@ -1,5 +0,0 @@
-var parser = require('node-markdown');
-
-var html = parser.Markdown('Using **Markdown** to test this file blahb blahb ablha here');
-
-console.log(html);
--- s1.js
+++
@@ -1,3 +0,0 @@
-var announce = require('./announce');
-
-announce('Something I wanted to say ha');
