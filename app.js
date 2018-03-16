60aa8243d7abb8d404fcdad205139d87e1d9ebb0
stefan22 <stefan.emailacct@gmail.com>
2018-03-16 21:23:19 +0000

    added new templating engine:ejs

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-//req routes
-var index = require('./routes/index');
-var users = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var locationRouter = require('./routes/locationRouter');
-var promoRouter = require('./routes/promoRouter');
+// require routes
+const index = require('./routes/index');
+const users = require('./routes/users');
+const dishRouter = require('./routes/dishRouter');
+const locationRouter = require('./routes/locationRouter');
+const promoRouter = require('./routes/promoRouter');
 
-var app = express();
+
+// express app instance
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// static server
 app.use(express.static(path.join(__dirname, 'public')));
 
-//route endpoints
+// routes endpoints
 app.use('/', index);
 app.use('/users', users);
-app.use('/dishes',dishRouter);
-app.use('/locations',locationRouter);
-app.use('/promotions',promoRouter);
+app.use('/dishes', dishRouter);
+app.use('/locations', locationRouter);
+app.use('/promotions', promoRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
